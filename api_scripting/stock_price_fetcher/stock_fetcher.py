039b3a3f56161573c270525325530d352e41c472
Fady Buni <fbuni@oakland.edu>
2025-12-30 11:58:22 -0500

    Add stock price fetcher using Alpha Vantage API

---
+++ api_scripting/stock_price_fetcher/stock_fetcher.py
@@ -0,0 +1,27 @@
+import requests
+
+API_KEY = "E2H8AU4PT920PWBT"
+welcome = input("Welcome to the stock fetcher app. Press any key to start (q to quit): ")
+
+while welcome != "q":
+    ticker = input("Please enter the ticker for the stock you would like to get the price of: ").strip().upper()
+    if ticker == "Q":
+        exit
+    url = "https://www.alphavantage.co/query"
+
+    params = {
+        "function" : "GLOBAL_QUOTE",
+        "symbol":ticker,
+        "apikey":API_KEY
+    }
+
+    response = requests.get(url,params=params,timeout=10)
+    response.raise_for_status()
+
+    data = response.json()
+    quote = data.get("Global Quote")
+    if not quote:
+        print("Not a valid ticker")
+        exit()
+    price = quote.get("05. price")
+    print(f"The price for the ticker {ticker} is ${price}")
